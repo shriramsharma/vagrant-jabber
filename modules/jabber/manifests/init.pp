7ac344f6eb597d34e56dfb1da93bd10741933a2e
Shriram <shriramsharma@ubuntusony.com>
2013-07-03 11:27:49 -0400

    Tweaked configuration. Port forwarding was the reason the client was not able to connect to the server

--- modules/jabber/manifests/init.pp
+++ modules/jabber/manifests/init.pp
@@ -6,14 +6,21 @@ package { "ejabberd":
 }
 
 file { "/etc/ejabberd/ejabberd.cfg":
-  mode => "766",
+  mode => "0640",
   owner => "root",
-  group => "root",
+  group => "ejabberd",
   source => "puppet:///modules/jabber/ejabberd.cfg",
 }
 
 service { "ejabberd":
   enable => true,
+  ensure => running,
+  hasstatus => true
+}
+
+exec { "setup_pwd":
+  command => "/usr/sbin/ejabberdctl register chadmin localhost test",
+  path => $path,
 }
 
 }
